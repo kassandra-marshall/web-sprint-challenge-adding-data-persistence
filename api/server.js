088fafe553977529370f10127f2acebc5ade3e90
dyzn3y <kdmarshall728@gmail.com>
2023-03-02 10:48:18 -0500

    completed package.json, index.js, server.js, and .env files

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
 // build your server here and require it from index.js
+const express = require('express');
+const server = express();
+
+server.use(express.json());
+
+server.use('*', (req, res, next) => { // eslint-disable-line
+    res.json({ api: 'up' })
+})
+
+server.use((error, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+        message: error.message,
+        stack: error.stack
+    });
+}); 
+
+module.exports = server;
