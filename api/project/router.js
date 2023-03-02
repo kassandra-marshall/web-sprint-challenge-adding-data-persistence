bb62071a98f958dfa96f60820c618ff264a3efa3
dyzn3y <kdmarshall728@gmail.com>
2023-03-02 11:21:58 -0500

    completed first migration

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,9 @@
 // build your `/api/projects` router here
+// const express = require('express');
+// const Projects = require('./model')
+
+// const router = express.Router()
+
+// router.get('/' (req, res, next) => {
+//     Promise.resolve()
+// })
